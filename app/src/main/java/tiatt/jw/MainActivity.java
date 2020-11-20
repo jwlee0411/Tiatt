db96697b6ea9d81557ba861931b26650a28a40d3
Jung haesung <48352078+XxCtrlZxX@users.noreply.github.com>
2020-11-20 12:49:40 +0900

    main layout scroll fixed

--- app/src/main/java/tiatt/jw/MainActivity.java
+++ app/src/main/java/tiatt/jw/MainActivity.java
@@ -30,10 +30,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        View decorView = getWindow().getDecorView();
-        // Hide the status bar. -> 굳이..? 뭐 이유가 있겠지
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
 
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
